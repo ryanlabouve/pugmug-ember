e4f80fc809eacf78eda2e3e8fd20ee14bfbadc64
Ryan LaBouve <imryanlabouve@gmail.com>
2015-02-12 01:18:26 -0600

    Polish

--- app/controllers/pugs.js
+++ app/controllers/pugs.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   currentPug: 1,
+  pugsCart: 0,
+  pugsInCart: function() {
+    return this.get('pugsCart');
+  }.property('pugsCart'),
   totalPugs: function() {
     return this.get('model').get('length');
   }.property('model'),
@@ -9,14 +13,19 @@ export default Ember.Controller.extend({
     return "margin-left: -" + ((this.get('currentPug') - 1) * 250) + "px";
   }.property('currentPug'),
   actions: {
+    skip: function() {
+      this.send('nextPug');
+    },
+    add: function() {
+      this.set('pugsCart', this.get('pugsCart') + 1);
+      this.send('nextPug');
+    },
     nextPug: function() {
       var next_pug_index;
       console.log(">>>>>" + this.get('currentPug'), this.get('totalPugs'))
       if( this.get('currentPug') + 1 <= this.get('totalPugs')) {
-        alert('next pug');
         next_pug_index = this.get('currentPug') + 1;
       } else {
-        alert('not next pug')
         next_pug_index = this.get('currentPug');
       }
       this.set('currentPug', next_pug_index);
